7e97452d7dd920ed912330497143176c9e8fdd90
Luis Santiago <luis.santiago56@upr.edu>
2023-08-01 12:54:51 -0600

    added view page logic and css styling with bootstrap.

--- bike-ui/src/app/app-routing.module.ts
+++ bike-ui/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { ViewRegistrationComponent } from './components/view-registration/view-r
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomeComponent
   },
   {
